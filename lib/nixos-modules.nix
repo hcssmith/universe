bb069db65f55b1a81a86c45e7e5a5e424804eecd
Hallam Smith <me@hcssmith.com>
2024-06-29 10:27:54 +0100

    feat(caching): Add nix-community cachix cache.

--- lib/nixos-modules.nix
+++ lib/nixos-modules.nix
@@ -24,7 +24,15 @@
   };
 
   nix-config = {
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
     registry = registry;
     package = pkgs.nix;
   };
